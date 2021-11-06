79f997b487363dbe403849d35bfa66db5eb0444e
Brandon Franks <bgfranks.dev@gmail.com>
2021-11-05 22:39:35 -0400

    created the login UI and handles the login functionality and sets the online status to online

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,51 @@
+import { useLogin } from '../../hooks/useLogin';
+
 // styles
+import { useState } from 'react';
 import './Login.css';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, isPending, error } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+          required
+        />
+      </label>
+      {!isPending && <button className='btn'>Log In</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className='error'>{error}</div>}
+    </form>
+  );
 }
