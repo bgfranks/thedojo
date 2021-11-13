1020b30926103e2515ea88172469bb016394dcb6
Brandon Franks <bgfranks.dev@gmail.com>
2021-11-13 14:47:04 -0500

    added the filtering functionality

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useCollection } from '../../hooks/useCollection';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 // components
 import ProjectList from '../../components/projectList/ProjectList';
@@ -8,14 +10,55 @@ import ProjectFilter from '../../components/projectFilter/ProjectFilter';
 import './Dashboard.css';
 
 export default function Dashboard() {
+  // grabs the project collection from the firestore
   const { documents, error } = useCollection('projects');
+
+  // grabs the current user
+  const { user } = useAuthContext();
+
+  // filter state
+  const [currentFilter, setCurrentFilter] = useState('all');
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case 'all':
+            return true;
+          case 'mine':
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case 'development':
+          case 'design':
+          case 'marketing':
+          case 'sales':
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
-      {documents && <ProjectFilter />}
-      {documents && <ProjectList projects={documents} />}
+      {documents && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          changeFilter={changeFilter}
+        />
+      )}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
